a81b5c48525a92aafdc0416c9733d9bb7004381d
KangHeeWon <kkhw1202@gmail.com>
2024-11-08 22:15:24 +0900

    modify vite config

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -5,16 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: '/githubpagetest/vue-project/',
+  plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
-  optimizeDeps: {
-    include: ["vue"]
   },
 })
